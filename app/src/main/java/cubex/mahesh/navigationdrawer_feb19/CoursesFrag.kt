53e987fecdb280e27a693dab436a5a2ca5701b7b
maheshthippala <mahesh.cubex@gmail.com>
2019-02-15 08:27:37 +0530

    Initial commit

---
+++ app/src/main/java/cubex/mahesh/navigationdrawer_feb19/CoursesFrag.kt
@@ -0,0 +1,18 @@
+package cubex.mahesh.navigationdrawer_feb19
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CoursesFrag : Fragment()
+{
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var v = inflater.inflate(R.layout.courses_frag,
+                                container,false)
+
+        return v
+    }
+}
